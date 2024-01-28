754528feed32095c2183c638740d4063f4c03914
Samuel-Toriola <samueloladipupo.toriola@gmail.com>
2024-01-28 22:26:28 +0100

    Adding tailwind colours

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Samuel</div>;
+  return <div className="hidden text-sm font-semibold">Samuel</div>;
 }
 
 export default Username;
