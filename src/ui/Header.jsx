c2346f11cb11562eabffc88cf2b61a6aaa13e105
Samuel-Toriola <samueloladipupo.toriola@gmail.com>
2024-02-05 17:30:42 +0100

    Adding tailwind

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className="border-b border-stone-200 bg-yellow-400 px-4 py-3  sm:px-6 flex items-center justify-between">
+    <header className="border-b border-stone-200 bg-yellow-400 px-4 py-3  sm:px-6 flex items-center justify-between font-sans">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
